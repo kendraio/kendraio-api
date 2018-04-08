3fc2e8850926104e0f2e345ad926ff3c0229fd14
neilharris2019@gmail.com <neilharris2019@gmail.com>
2018-04-08 09:24:05 +0100

    added instructions for how to get hold of the public key in a format that actually works

--- api-jwt-test.py
+++ api-jwt-test.py
@@ -1,8 +1,16 @@
-import os, jwt, sys
+import os, jwt, sys, base64
 
 # Note:  the JWT public key needs to be put in an environment variable called JWT_PUBLIC_KEY
-#        this is the string value extracted from a JWKS, for example one found from auth0
-#        see https://auth0.com/docs/api-auth/tutorials/verify-access-token#how-can-i-verify-the-signature-
+#        the contents of this will be a PEM formatted file containing only the public key
+#        you can recognise these because they start with a line saying "-----BEGIN PUBLIC KEY----"
+#
+#        command to extract from a PEM-formatted certificate:
+#
+#            openssl x509 -pubkey -noout -in cert.pem
+#
+#        to get a PEM-formatted certificate file from Auth0
+#
+#            https://[yourdomain].eu.auth0.com/.well-known/jwks.json
 
 public_key = os.environ["JWT_PUBLIC_KEY"]
 jwt.decode(sys.argv[1], public_key, algorithms=['RS256'])
